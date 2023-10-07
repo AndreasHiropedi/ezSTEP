1034d4eac11844523ff5412154c0a1f3430889a5
AndreasHiropedi <s2015345@ed.ac.uk>
2023-10-07 20:19:43 +0100

    added more functionality

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import base64
-from dash import Dash, html
+from dash import Dash, html, dcc, Input, Output, callback
 
-app = Dash(__name__)
+app = Dash(__name__, suppress_callback_exceptions=True)
 
 app.layout = html.Div(
     children=[
@@ -59,10 +59,64 @@ app.layout = html.Div(
 
         # Main page content
         html.Div(
-            id='app-page-content'
+            children=[
+                # Text part explaining how the app works
+                html.Div(
+                    id='user-guide',
+                    children=[
+                        html.H3("Here is how to use the app")
+                    ]
+                ),
+
+                # Input tabs
+                dcc.Tabs(
+                    id='tabs-container',
+                    className='tabs-container',
+                    value="model input parameters",
+                    children=[
+                        dcc.Tab(
+                            id='tab',
+                            label="Model input parameters",
+                            value="model input parameters"
+                        ),
+                        dcc.Tab(
+                            id='tab',
+                            label="Upload datasets",
+                            value="upload datasets"
+                        ),
+                        dcc.Tab(
+                            id='tab',
+                            label="Visualize model outputs",
+                            value="visualize model outputs"
+                        )
+                    ]
+                ),
+
+                # Output of the tabs
+                html.Div(id='tabs-content')
+            ],
         )
     ],
 )
+
+
+@callback(Output('tabs-content', 'children'),
+          Input('tabs-container', 'value'))
+def render_content(tab):
+    if tab == 'model input parameters':
+        return html.Div([
+            html.H3('User input tabs')
+        ])
+    elif tab == 'upload datasets':
+        return html.Div([
+            html.H3('Upload boxes for datasets')
+        ])
+    elif tab == "visualize model outputs":
+        return html.Div([
+            html.H3('Model outputs')
+        ])
+    return 'No content available.'
+
 
 if __name__ == '__main__':
     app.run_server()
