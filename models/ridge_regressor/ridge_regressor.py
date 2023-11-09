63b1646ba5858753f638420ca68b676b505c7614
AndreasHiropedi <s2015345@ed.ac.uk>
2023-11-09 09:56:34 +0000

    added skeletons for all models

---
+++ models/ridge_regressor/ridge_regressor.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+
+
+class RidgeRegressor:
+
+    def __init__(self):
+        pass
+
+    def train_model(self):
+        pass
+
+    def test_model(self):
+        pass
+
+    def query_model(self):
+        pass
+
+    def hyper_opt(self):
+        pass
