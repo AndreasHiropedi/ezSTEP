caa9114afa18f72b028a492e879121f60117defa
unknown <anhiropedi@nihito.io>
2025-08-16 15:52:48 +0200

    major restructuring

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from .random_forest import RandomForest
-from .ridge_regressor import RidgeRegressor
-from .multilayer_perceptron import MultiLayerPerceptron
-from .support_vector_machine import SupportVectorMachine
--- local_version/app/globals.py
+++
@@ -1,8 +0,0 @@
-# variables to store the user provided data
-TRAINING_DATA = None
-TRAINING_FILE = None
-TESTING_DATA = None
-TESTING_FILE = None
-QUERYING_DATA = None
-QUERYING_FILE = None
-MODELS_LIST = {'Model 1': None}
